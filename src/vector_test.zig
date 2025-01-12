443092269a72ce1d0c2c7edda55a72cae8226da2
athul-ramesh <athulinc@gmail.com>
2025-01-12 23:41:25 +0530

    vector

---
+++ src/vector_test.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+const expectEqual = std.testing.expectEqual;
+test "Basic vector usage" {
+    // Vectors have a compile-time known length and base type.
+    const a = @Vector(4, i32){ 1, 2, 3, 4 };
+    const b = @Vector(4, i32){ 5, 6, 7, 8 };
+
+    // Math operations take place element-wise.
+    const c = a + b;
+
+    // Individual vector elements can be accessed using array indexing syntax.
+    try expectEqual(6, c[0]);
+    try expectEqual(8, c[1]);
+    try expectEqual(10, c[2]);
+    try expectEqual(12, c[3]);
+}
